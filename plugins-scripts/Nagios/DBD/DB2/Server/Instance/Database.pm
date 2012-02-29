6552c46f64577480b3cbdc81d337581ea4001444
Gerhard Lausser <gerhard.lausser@consol.de>
2012-02-29 10:11:25 +0100

    fix a bug in connected-users

--- plugins-scripts/Nagios/DBD/DB2/Server/Instance/Database.pm
+++ plugins-scripts/Nagios/DBD/DB2/Server/Instance/Database.pm
@@ -190,10 +190,10 @@ sub init {
           ($self->{rows_selected} / $self->{rows_read} * 100) : 100;
     }
   } elsif ($params{mode} =~ /server::instance::database::connectedusers/) {
+    #SELECT COUNT(*) FROM sysibmadm.applications WHERE appl_status = 'CONNECTED'
+    # there are a lot more stati than "connected". Applications can
+    # be connected although being in another state.
     $self->{connected_users} = $self->{handle}->fetchrow_array(q{
-        #SELECT COUNT(*) FROM sysibmadm.applications WHERE appl_status = 'CONNECTED'
-        # there are a lot more stati than "connected". Applications can
-        # be connected although being in another state.
         SELECT COUNT(*) FROM sysibmadm.applications
     });
   } elsif ($params{mode} =~ /server::instance::database::lastbackup/) {
